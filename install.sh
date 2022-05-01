48a24524617507a34154d48eaa1c84d0e2b7236a
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-05-01 12:38:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 sh clean.sh
-mkdir lib/students
-mkdir lib/admin
-mkdir lib/faculty
-mkdir lib/coursedata
+mkdir lib/students lib/admin lib/faculty lib/coursedata
 g++ -o aims aims_main.cpp -I include src/*.cpp -Wall -g -O2 -std=c++11
