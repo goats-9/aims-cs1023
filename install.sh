90e6697d8a87f793860454fa5bbab84ab724255a
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-05-04 11:39:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sh clean.sh
+mkdir lib
 mkdir lib/students lib/admin lib/faculty lib/coursedata
 mkdir lib/students/courses lib/students/secrets lib/faculty/courses lib/faculty/secrets lib/admin/courses lib/admin/secrets 
 g++ -o aims aims_main.cpp -I include src/*.cpp -Wall -g -O2 -std=c++11
