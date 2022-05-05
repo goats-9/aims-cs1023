a61969fa4acc0bbd4433242ee6acc4d3526e11bd
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-05-05 16:05:45 +0530

    Update globvars.hpp

--- include/globvars.hpp
+++ include/globvars.hpp
@@ -4,7 +4,8 @@
 #endif
 // For backtraces
 #define err_open(A, fname) if (!A.is_open()) {A.close(); fprintf(stderr, "[ERROR] %s:%d: Failed to open %s.\n", __FILE__, __LINE__, fname.c_str()); goto error;}   // File not opened
-#define err_main(A, M, ...) if (!A) {fprintf(stderr, "[ERROR] %s:%d" M "\n", __FILE__, __LINE__, __VA_ARGS__); continue;}     // Main program does not terminate
+#define err_ret(A, M, ...) if (!A) {fprintf(stderr, "[ERROR] %s:%d" M "\n", __FILE__, __LINE__, ##__VA_ARGS__); goto error;}
+#define err_main(A, M, ...) if (!A) {fprintf(stderr, "[ERROR] %s:%d" M "\n", __FILE__, __LINE__, ##__VA_ARGS__); continue;}     // Main program does not terminate
 
 #include <bits/stdc++.h>
 using namespace std;
