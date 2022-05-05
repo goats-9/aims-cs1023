2a0a53cd478923409b613b0ed769c04211e062d1
goats-9 <102507378+goats-9@users.noreply.github.com>
2022-05-05 06:59:54 +0530

    Update globvars.hpp

--- include/globvars.hpp
+++ include/globvars.hpp
@@ -3,7 +3,7 @@
 #define _GLOBVARS_H_
 #endif
 // For backtraces
-#define err_ret(A, M, ...) if (!A) {fprintf(stderr, "[ERROR] %s:%d: " M "\n", __FILE__, __LINE__, __VA_ARGS__); return -1;}   // Exit value
+#define err_ret(A, M, ...) if (!A) {A.close(), fprintf(stderr, "[ERROR] %s:%d: " M "\n", __FILE__, __LINE__, __VA_ARGS__); return -1;}   // Exit value
 #define err_main(A, M, ...) if (!A) {fprintf(stderr, "[ERROR] %s:%d" M "\n", __FILE__, __LINE__, __VA_ARGS__); continue;}     // Main program does not terminate
 
 #include <bits/stdc++.h>
